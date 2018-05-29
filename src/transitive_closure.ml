6636bafee1243c8fa0350ea7416cb0ffe1bd1360
brubar <benoit.razet@gmail.com>
2018-05-29 06:01:51 -0400

    add test for transitive closure algo

--- src/transitive_closure.ml
+++ src/transitive_closure.ml
@@ -242,35 +242,4 @@ module IntervalIsSemi = struct
   let star = Interval.interval_star;;
 end;; (*IntervalIsSemi*)
 
-
 module GraphInterval = Closure(IntervalIsSemi);;
-
-
-let example1 =
-  let tab = Array.make_matrix 5 5 IntervalIsSemi.zero in
-  let inter0 = Interval.Between (Interval.Val 0,Interval.Val 0) in
-  let inter1 = Interval.interval_d1 in
-  let () =
-    tab.(0).(1) <- inter1
-    ; tab.(0).(2) <- inter1
-    ; tab.(1).(2) <- inter1
-    ; tab.(1).(3) <- inter1
-    ; tab.(3).(4) <- inter1
-    ; tab.(4).(3) <- inter1
-
-    (*; tab.(0).(0) <- inter0
-    ; tab.(1).(1) <- inter0
-    ; tab.(2).(2) <- inter0
-    ; tab.(3).(3) <- inter0
-      ; tab.(4).(4) <- inter0*)
-
-  in
-  tab
-;;
-
-let res1 = GraphInterval.closure_tab (5,example1);;
-
-let test () = 
-  GraphInterval.iter_mat2 5 (fun x y -> Interval.print_interval res1.(x).(y)) print_newline print_newline;;
-
-(*test () *)
